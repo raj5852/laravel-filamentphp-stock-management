3831459c2a414a51f9437a2824855b8ced240d1f
Raj <raj@Rajs-MacBook-Air.local>
2025-06-04 16:28:22 +0600

    rename class

--- app/Livewire/ReceivefromCustomer.php
+++ app/Livewire/ReceivefromCustomer.php
@@ -16,7 +16,7 @@ use Filament\Tables\Table;
 use Illuminate\Support\HtmlString;
 use Livewire\Component;
 
-class ReceivefromCustomer extends Component implements HasForms, HasTable
+class ReceiveFromCustomer extends Component implements HasForms, HasTable
 {
     use InteractsWithForms;
     use InteractsWithTable;
@@ -56,7 +56,7 @@ class ReceivefromCustomer extends Component implements HasForms, HasTable
                 TextColumn::make('amount')->label('Amount')->getStateUsing(function ($record) {
                     return number_format($record->amount, 2, '.', '');
                 })->summarize(
-                    Sum::make()->formatStateUsing(fn ($state) => number_format($state, 2, '.', '').' Tk')->label('Total')
+                    Sum::make()->formatStateUsing(fn($state) => number_format($state, 2, '.', '') . ' Tk')->label('Total')
                 ),
             ])
 
