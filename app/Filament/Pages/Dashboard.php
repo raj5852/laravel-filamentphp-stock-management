954698a7cb76e31e42f0efd87978bdad71f60463
Raj <raj@Rajs-MacBook-Air.local>
2025-06-04 14:21:04 +0600

    added dashboard permission

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard  extends BaseDashboard
+{
+    public static function canAccess(): bool
+    {
+        return auth()->user()->can('dashboard');
+    }
+}
